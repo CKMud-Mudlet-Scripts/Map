5073d8d4a97509ce4efde15296f5c24bae2fd088
Jason Fried <me@jasonfried.info>
2024-08-13 16:50:04 -0700

    per wilds/city map objects

--- src/scripts/Create_Map.lua
+++ src/scripts/Create_Map.lua
@@ -1,10 +1,16 @@
 local fried = require("__PKGNAME__.fried")
 local map = fried:get_table("map")
+local helpers = fried:get_table("map.helpers")
+local city = fried:get_table("map.city")
+local wilds = fried:get_table("map.wilds")
+# Default to city map
+map.picked = map.picked or 1
+map.hide = map.hide or false
 
-local function create_container()
+local function create_container(name)
   local default_constraints =
     {
-      name = "MapContainer",
+      name = f"{name}MapContainer",
       x = "75.78%",
       y = "25.41%",
       width = "23.5%",
@@ -23,11 +29,11 @@ local function create_container()
   return Adjustable.Container:new(default_constraints)
 end
 
-local function create_console()
-  console =
+local function create_console(name, container)
+  local console =
     Geyser.MiniConsole:new(
       {
-        name = "MapConsole",
+        name = f"{name}MapConsole",
         x = 0,
         y = "1%",
         autoWrap = false,
@@ -37,10 +43,45 @@ local function create_console()
         width = "100%",
         height = "99%",
       },
-      map.container
+      container
     )
   return console
 end
 
-map.container = map.container or create_container()
-map.console = map.console or create_console()
+city.container = city.container or create_container("City")
+city.console = city.console or create_console("City", city.container)
+wilds.container = wilds.container or create_container("Wilds")
+wilds.console = wilds.console or create_console("Wilds", wilds.container)
+
+local kind_saves_loc = {
+  f"{getMudletHomeDir()}/__PKGNAME__/_saveCityMap.lua",
+  f"{getMudletHomeDir()}/__PKGNAME__/_saveWildsMap.lua",
+}
+
+local kind = {
+  city.container,
+  wilds.container,
+}
+
+function helpers:show()
+  
+end
+
+function helpers:hide()
+end
+
+function helpers:save()
+end
+
+function helpers:load()
+end
+
+function helpers:pick(kind)
+  if not map.hide then
+  if kind == 1 then
+    wilds.container. 
+  else
+   
+  end
+end
+end
